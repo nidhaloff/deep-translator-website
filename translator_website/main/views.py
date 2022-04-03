2f0400efec19ad165377a7a1a096ccbbe3f54e5e
Sam sunder <samsunderch@gmail.com>
2022-04-03 21:46:36 +0530

    Initial commit

---
+++ translator_website/main/views.py
@@ -0,0 +1,31 @@
+import imp
+from django.shortcuts import render
+from deep_translator import (GoogleTranslator,
+                             MicrosoftTranslator,
+                             PonsTranslator,
+                             LingueeTranslator,
+                             MyMemoryTranslator,
+                             YandexTranslator,
+                             PapagoTranslator,
+                             DeepL,
+                             QCRI,
+                             single_detection,
+                             batch_detection)
+from django.http import JsonResponse
+import json
+import iso639
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def translate(request):
+    if request.method == "POST":
+        target_language = request.POST.get('tar-lang');
+        if request.POST.get('text'):
+            translation = GoogleTranslator(target=target_language).translate(request.POST.get('text', ''))
+        else:
+            translation = ""
+        payload = {'result': translation}
+        return JsonResponse(payload)
