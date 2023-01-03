9f80665f6f4f3bbb4cb39808e2d8ea074abb5186
ArthurGossel1 <pro.arthur.gosselin@gmail.com>
2023-01-03 13:04:10 +0100

    DYNAMIC REQUIRED PARAMS

--- translator_website/main/views.py
+++ translator_website/main/views.py
@@ -1,3 +1,4 @@
+from dotenv import dotenv_values
 from django.shortcuts import render
 from deep_translator import (
     GoogleTranslator,
@@ -14,6 +15,7 @@ from deep_translator import (
 )
 from django.http import JsonResponse
 
+config = dotenv_values(".env")
 
 # Create your views here.
 def home(request):
@@ -67,6 +69,7 @@ def translate(request):
         target_language = request.POST.get('target')
         translator = request.POST.get('translator')
         source_language = request.POST.get('source')
+        print("target : " + target_language + " source : " + source_language + " translator : " + translator)
 
         # Default translator is google
         if not translator:
@@ -82,14 +85,29 @@ def translate(request):
         if text:
             # Check if all params are present (depends on current translator)
             missing_params = []
+            params_to_give = [source_language]
+            if target_language is not None:
+                params_to_give.append(target_language)
             for attr in translators[translator]["requiredAttr"] or []:
-                if not request.POST.get(attr):
-                    missing_params.append(attr)
+                cur_atr = request.POST.get(attr)
+                if not cur_atr:
+                    # Check if current param is in .env
+                    print("PARAM IN ENV ? : " + config.get("DEEPL_API_KEY"))
+                    env_param = config.get("DEEPL_API_KEY")
+                    if env_param is None:
+                        missing_params.append(attr)
+                    else:
+                        if env_param not in params_to_give:
+                            params_to_give.append(env_param)
+                else:
+                    if cur_atr not in params_to_give:
+                        params_to_give.append(cur_atr)
 
             if len(missing_params) > 0:
                 return JsonResponse({'error': 'missing attribute: ' + ", ".join(missing_params)})
+            print(params_to_give)
 
-            target_translator = translators[translator]["translator"](source=source_language, target=target_language)
+            target_translator = translators[translator]["translator"](*params_to_give)
             translation = target_translator.translate(text)
         else:
             translation = ""
